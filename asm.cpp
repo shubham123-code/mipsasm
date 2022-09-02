30c9350fc3e99dd0544e557bd7dcd45a44cc37be
shubham <maple@maples-MacBook-Pro.local>
2022-09-02 19:24:36 +0530

    added hi

--- asm.cpp
+++ asm.cpp
@@ -1,8 +1,11 @@
-#include <iostream>
-
+#include "/Users/maple/Downloads/stdc++.h/stdc++.h"
+#include <fstream>
 using namespace std;
 
 int main(){
-    cout<<"Hello World!!!";
+    ifstream myfile("sum_of_n_natural_nums.txt");
+    stringstream buffer;
+    buffer<<myfile.rdbuf();
+    cout<<buffer.str();
     return 0;
 }
