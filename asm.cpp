02d757330768c25caa5c7bd5628214026e25d34a
shubham <maple@maples-MacBook-Pro.local>
2022-09-03 23:52:35 +0530

    add

--- asm.cpp
+++ asm.cpp
@@ -2,10 +2,53 @@
 #include <fstream>
 using namespace std;
 
+stringstream buffer;
+ifstream myfile("sum_of_n_natural_nums.txt");
+string file;
+void readfile(){
+    if(!myfile){
+        cout<<"File is empty !!\n";
+    }
+    buffer<<myfile.rdbuf();
+    file = buffer.str();
+    
+}
+map<string,int>m;
+
+vector<string>tokens;
+
+void get_tokens(){
+    string temp;
+    int f=0;
+    for(int i=0;i<file.size();i++){
+        if(file[i]=='"'){
+            if(!f)f=1;
+            else f=0;
+        }
+        if(!f&&(file[i]==' '||file[i]=='\n')){
+            if(temp.size()){
+                tokens.push_back(temp);
+                cout<<temp<<"\n";
+            }
+            temp.clear();
+            continue;
+        }
+        temp.push_back(file[i]);
+    }
+}
+// unordered_map<string,int> arguments={
+//     {"add",1}
+// };
+
+
 int main(){
     ifstream myfile("sum_of_n_natural_nums.txt");
-    stringstream buffer;
-    buffer<<myfile.rdbuf();
-    cout<<buffer.str();
+    readfile();
+    get_tokens();
+    if(!file.size()){
+        cout<<"File is empty\n";
+        return 0;
+    }
+
     return 0;
 }
