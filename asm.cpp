1e8a6000f437eb28458f580e785fe4342b17b2a5
shubham <maple@maples-MacBook-Pro.local>
2022-09-16 13:02:09 +0530

    finally completegit add .

--- asm.cpp
+++ asm.cpp
@@ -2,6 +2,7 @@
 
 #include "stdc++.h"
 //#include <bits/stdc++.h>
+
 #include <fstream>
 using namespace std;
 
@@ -27,6 +28,44 @@ map<string, int> label;
 map<string, int> label_data;
 vector<string> lines;
 int errs = 0;
+
+// separate the input file into lines and stores them in a vector of strings
+void trim()
+{
+    string newFile;
+    int f = 0;
+    int cnt = 0;
+    for (int i = 0; i < file.size(); i++)
+    {
+        if (file[i] == ';')
+        {
+            f = 1;
+            continue;
+        }
+    
+        if (file[i] == '\n' && f == 1 )
+        {
+            f = 0;
+            if(newFile.back()!=':')newFile.push_back('\n');
+            cnt++;
+            continue;
+        }
+        if (f)
+            continue;
+        if (file[i] == '\n' && newFile.back() == '\n')
+            continue;
+        if (newFile.back() == ':' && (file[i] == ' ' || file[i] == '\n'||file[i]=='\t'))
+            continue;
+        if (newFile.back() == ':')
+            newFile.push_back(' ');
+        if (file[i] == '\n')
+            cnt++;
+        
+        newFile.push_back(file[i]);
+    }
+    cout<<newFile;
+    file = newFile;
+}
 void get_line()
 {
     string temp;
@@ -47,8 +86,11 @@ void get_line()
                 r = 1;
             if (temp.size())
             {
+                reverse(temp.begin(),temp.end());
+                while(temp.back()==' ')temp.pop_back();
+                reverse(temp.begin(),temp.end());
                 lines.push_back(temp);
-                // cout<<temp<<"\n";
+                
             }
             temp.clear();
             continue;
@@ -57,10 +99,14 @@ void get_line()
     }
     if (temp.size())
     {
+        reverse(temp.begin(),temp.end());
+        while(temp.back()==' ')temp.pop_back();
+        reverse(temp.begin(),temp.end());
         lines.push_back(temp);
     }
 }
 
+// separates tokens from an input line
 vector<string> get_tokens(string line)
 {
     string temp;
@@ -68,14 +114,14 @@ vector<string> get_tokens(string line)
     vector<string> tokens;
     for (int i = 0; i < line.size(); i++)
     {
-        if (!f && (line[i] == ' ' || line[i] == ':'))
+        if (!f && (line[i] == ' ' || line[i] == ':'|| line[i]=='\t'))
         {
             if (line[i] == ':')
                 temp.push_back(':');
             if (temp.size())
             {
                 tokens.push_back(temp);
-                // cout<<temp<<" ";
+                
             }
             temp.clear();
             continue;
@@ -86,6 +132,7 @@ vector<string> get_tokens(string line)
     return tokens;
 }
 
+// stores label address into a map
 void set_labels()
 {
     int pc = 0;
@@ -100,38 +147,35 @@ void set_labels()
         string s;
         for (int i = 0; i < tokens.size(); i++)
         {
-            if (tokens[i] == "data")
+            if (tokens[i] == "data"||tokens[i]== "SET")
             {
                 if (i != 0)
                 {
                     stringstream val_s(tokens[i + 1]);
                     int val;
                     val_s >> val;
-                    label_data[tokens[i - 1]] = val;
-                    pc++;
-                    continue;
+                    label_data[tokens[i - 1]] = pc;
+                    break;
                 }
             }
         }
         if (tokens[c].back() == ':')
         {
-            if (tokens[c] == "label:")
-            {
-                log_file << "label declaration 'label:' not allowed. Choose a different name"
-                         << " (line " << i + 1 << ")\n";
-                errs = 1;
-            }
             tokens[c].pop_back();
             if (label.find(tokens[c]) != label.end())
             {
                 log_file << "duplicate label found: " << tokens[c] << " (line " << i + 1 << ")\n";
                 errs = 1;
             }
-            label[tokens[c]] = pc + 1;
+            label[tokens[c]] = pc;
         }
-        pc++;
+
+        if (tokens.size() >= 1 && tokens.back().back() != ':')
+            pc++;
     }
 }
+
+// binary string to hex convertor
 string bin_hex(string s)
 {
     string k;
@@ -149,8 +193,10 @@ string bin_hex(string s)
     }
     return k;
 }
+// main function
 int main(int argc, char *argv[])
 {
+    // setting names of the output files
     myfile.open(argv[1]);
     char out_name[100];
     strcpy(out_name, argv[1]);
@@ -158,10 +204,12 @@ int main(int argc, char *argv[])
     out_name[sz - 2] = '\0';
     out_name[sz - 3] = 'o';
 
-    o_file.open(out_name);
+    o_file.open(out_name, ios::out | ios::binary);
     out_name[sz - 3] = 'l';
     listing.open(out_name);
     readfile();
+    trim();
+    // storing instruction codes for each instructions into a map
     commands["ldc"] = 0;
     commands["adc"] = 1;
     commands["ldl"] = 2;
@@ -183,9 +231,20 @@ int main(int argc, char *argv[])
     commands["HALT"] = 18;
     commands["data"] = 19;
     commands["SET"] = 20;
+
+    // initialising the process
     get_line();
     set_labels();
-    int pc = 1;
+    if(errs){
+        if (errs)
+        {
+            cout<<"errors in file check the log file to the errors.\n";
+            o_file.clear();
+            listing.clear();
+            
+        }
+    }
+    int pc = 0;
     vector<string> obj;
 
     for (int i = 0; i < lines.size(); i++)
@@ -193,50 +252,81 @@ int main(int argc, char *argv[])
         vector<string> tokens = get_tokens(lines[i]);
         if (tokens.back().size() == 0)
             tokens.pop_back();
-
-        int c = 0;
-
-        for (auto u : tokens)
+        if (tokens.size() == 1 && tokens[i].back() == ':')
         {
-            cout << u << "|";
+            continue;
         }
-        cout << "\n";
+        int c = 0;
+        
+        // cout << "\n";
         string s;
+        // increase iterator incase encounter a label
         if (tokens[c].back() == ':')
         {
             c++;
+            
+        }
+        for(auto u:tokens){
+            cout<<u<<"|";
         }
         int cnt = 0;
         string s1;
-        while (c < tokens.size())
+        int branch=0;
+        if(tokens[c]=="data"||tokens[c]=="SET"){
+            int val=stoi(tokens[c+1],nullptr,10);
+            for(int i=31;i>=0;i--){
+                s1.push_back('0'+(val>>i&1));
+            }
+            branch=1;
+        }
+        // processing the input string
+        while (c < tokens.size()&&!branch)
         {
-            while (tokens[c].back() == ' ')
+            while (tokens[c].back() == ' '||tokens[c].back()=='\t')
                 tokens[c].pop_back();
             if (cnt == 1)
             {
                 int val = 0;
-                if (tokens[c].back() >= 'a')
+                if (label_data.find(tokens[c] + ":") != label_data.end()||label.find(tokens[c]) != label.end())
                 {
+                    // separate block for data operation
                     if (label_data.find(tokens[c] + ":") != label_data.end())
                     {
-                        cout << tokens[c] << "\n";
+                        // cout << tokens[c] << "\n";
                         val = label_data[tokens[c] + ":"];
-                        cout << val << "\n";
+                        // cout << val << "\n";
                     }
                     else
                     {
+                        // incase no data operation
                         if (label.find(tokens[c]) == label.end())
                         {
                             log_file << "invalid label: " << tokens[c] << " (line " << i + 1 << ")\n";
                             errs = 1;
                         }
-                        val = label[tokens[c]];
+                        val = label[tokens[c]] - pc - 1;
                     }
                 }
                 else
                 {
-                    stringstream val_s(tokens[c]);
-                    val_s >> val;
+                    //converting opcode to integer
+
+                    if(tokens[c].size()<2||tokens[c][1]!='x'){
+                        int f=0;
+                        for(auto u:tokens[c]){
+                            if(u-'0'>9){
+                                f=1;
+                            }
+                        }
+                        if(!f)val=stoull(tokens[c],nullptr,10);
+                        else{
+                            log_file<<"input is not an integer: "<<tokens[c]<<"\n";
+                        }
+                    }
+                    else {
+                        val = stoull(tokens[c],nullptr,16);
+                    }
+                    
                 }
                 for (int j = 23; j >= 0; j--)
                 {
@@ -262,11 +352,12 @@ int main(int argc, char *argv[])
             cnt++;
             c++;
         }
-
-        if (s.size() == 0)
+        // converting string to asm file
+        if (s.size() == 0&&s1.size()==8)
         {
             s = string(24, '0');
         }
+        string value=s;
         s.append(s1);
         string pc1;
         string temp;
@@ -281,19 +372,31 @@ int main(int argc, char *argv[])
                 temp.push_back('0');
             }
         }
-        pc1 = bin_hex(temp);
-        int sz = pc1.size();
-        pc1 = string(8 - sz, '0') + pc1;
-        string lis = pc1 + " " + bin_hex(s) + " " + lines[i];
-        listing << lis << "\n";
-        o_file << s << "\n";
-        cout << s << "\n";
+        if(!errs){
+            pc1 = bin_hex(temp);
+            int sz = pc1.size();
+            pc1 = string(8 - sz, '0') + pc1;
+            if(tokens[0].back()==':'){
+                listing<<pc1<<" "<<tokens[0]<<"\n";
+            }
+            string lis;
+            if(!branch)lis = pc1 + " " + bin_hex(s) + " " + tokens[cnt-1] + " 0x"  + bin_hex(value);
+            else lis=pc1 + " " + bin_hex(s) + " data 0x" + bin_hex(s); 
+            listing << lis << "\n";
+            int t = stoul(s, nullptr, 2);
+            o_file.write((char *)&t, sizeof(t));
+        }
+       
         pc++;
     }
     if (errs)
     {
+        cout<<"errors in file check the log file to the errors.\n";
         o_file.clear();
         listing.clear();
+    }
+    else{
+        log_file<<"no errors found.\n";
     }
     o_file.close();
     log_file.close();
--- emu.cpp
+++ emu.cpp
@@ -1,32 +1,281 @@
 #include "stdc++.h"
 #include <fstream>
-
+#define li long int
 using namespace std;
 
 ifstream myfile;
 string file;
 ofstream output;
+vector<li>instructions;
+int SP, PC, A, B;
 
-void read_file(){
-    if(!myfile){
-        cout<<"file is empty";
+// Memory with 2^24 addresses
+int memory[16777216]; 
+
+char* mnemonic[] = {"ldc", "adc", "ldl", "stl", "ldnl", "stnl", "add", "sub", "shl", "shr", "adj", "a2sp", "sp2a", "call", "return", "brz", "brlz", "br", "HALT"};
+
+void read_file()
+{
+    char y[100];
+    if (!myfile)
+    {
+        cout << "file is empty";
         return;
     }
-    stringstream buffer;
-    buffer<<myfile.rdbuf();
-    file = buffer.str();
-    cout<<file<<"\n";
+    int line;
+
+    while (myfile.read((char *)&line, sizeof(line))){
+        instructions.push_back(line);
+    }
 }
 
-void get_line(string s){
-
+void ldc(int value) {
+	B = A;
+	A = value;
 }
 
+void adc(int value) {
+	A += value;
+}
 
-int main(int argc, char* argv[]){
-    myfile.open(argv[1]);
+void ldl(int offset) {
+	B = A;
+	A = memory[SP + offset];
+}
+
+void stl(int offset) {
+	memory[SP + offset] = A;
+	A = B;
+}
+
+void ldnl(int offset) {
+	A = memory[A + offset];
+}
+
+void stnl(int offset) {
+	memory[A + offset] = B;
+}
+
+void add() {
+	A += B;
+}
+
+void sub() {
+	A = B - A;
+}
+
+void shl() {
+	A = B << A;
+}
+
+void shr() {
+	A = B >> A;
+}
+
+void adj(int value) {
+	SP += value;
+}
+
+void a2sp() {
+	SP = A;
+	A = B;
+}
+
+void sp2a() {
+	B = A;
+	A = SP;
+}
+
+void call(int offset) {
+	B = A;
+	A = PC;
+	PC += offset;
+}
+
+void RETURN() {
+	PC = A;
+	A = B;
+}
+
+void brz(int offset) {
+	if (A == 0) PC += offset;
+}
+
+void brlz(int offset) {
+	if (A < 0) PC += offset;
+}
+
+void br(int offset) {
+	PC += offset;
+} 
+char* getType(int mnem) {
+	if (mnem < 4 || mnem == 10) return "value";
+	else if (mnem == 4 || mnem == 5) return "value";
+	else if (mnem == 15 || mnem == 17) return "offset";
+	else if (mnem == 13 || mnem == 16) return "offset";
+	return "";
+}
+
+// Print ISA
+void isa() {
+	fprintf(stderr, "Opcode\tMnemonic\tOperand\n");
+	for (int i = 0; i <= 18; i++) {
+		fprintf(stderr, "%d\t%s\t%s\n", i, mnemonic[i], getType(i));
+	}
+	fprintf(stderr, "\tSET\tvalue\n");
+	return;
+}
+
+// Memory Dump until the number on instructions/data provided
+void printMemory(int line_num, bool isBefore) {
+	if (isBefore) fprintf(stderr, "\nMemory dump before execution\n");
+	else fprintf(stderr, "\nMemory dump after execution\n");
+	for (int i = 0; i < line_num; i++) {
+		if (i % 4 == 0) fprintf(stderr, "\n%08x ", i);
+		fprintf(stderr, "%08x ", memory[i]);
+	}
+	fprintf(stderr, "\n");
+	return;
+}
+
+// Memory Dump until the number on instructions/data provided
+void printMemoryToFile(int line_num, ofstream& file) {
+	for (int i = 0; i < line_num; i++) {
+		if (i % 4 == 0) file<<"\n%08x "<<i;
+		file<<"%08x "<<memory[i];
+	}
+	file<<"\n";
+	return;
+}
+
+// Convert 32 bit binary string to number
+int binToNumber(char* bin) {
+	int number = 0;
+	for (int i = 0; i < 32; i++) {
+		number *= 2;
+		if (bin[i] == '1') number += 1; 
+	}
+	return number;
+}
+
+// Read machine code into memory
+
+
+// Extract mnemonic code from instruction
+int getMnemonicCode(int instruction) {
+	return ((1 << 8) - 1) & instruction;
+}
+
+// Extract mnemonic code from instruction
+int getOperand(int instruction) {
+	return (instruction >> 8);
+}
+
+// Prints trace details
+void printTrace(int opcode, int operand) {
+	fprintf(stderr, "PC=%08x, SP=%08x, A=%08x, B=%08x %s\t%08x\n", PC, SP, A, B, mnemonic[opcode], operand);
+}
+
+// Print executable options
+void printOptions() {
+	fprintf(stderr, "usage: emu [options] file.o\n");
+	fprintf(stderr, "-trace\tshow instruction trace\n");
+	fprintf(stderr, "-before\tshow memory dump before execution\n");
+	fprintf(stderr, "-after\tshow memory dump after execution\n");
+	fprintf(stderr, "-isa\tdisplay ISA\n");
+}
+
+// Maps opcode to required function
+int executeInstruction(int opcode, int operand) {
+	if (opcode == 0) ldc(operand);
+	if (opcode == 1) adc(operand);
+	if (opcode == 2) ldl(operand);
+	if (opcode == 3) stl(operand);
+	if (opcode == 4) ldnl(operand);
+	if (opcode == 5) stnl(operand);
+	if (opcode == 6) add();
+	if (opcode == 7) sub();
+	if (opcode == 8) shl();
+	if (opcode == 9) shr();
+	if (opcode == 10) adj(operand);
+	if (opcode == 11) a2sp();
+	if (opcode == 12) sp2a();
+	if (opcode == 13) call(operand);
+	if (opcode == 14) RETURN();
+	if (opcode == 15) brz(operand);
+	if (opcode == 16) brlz(operand);
+	if (opcode == 17) br(operand);
+	else return 0;
+	return 1;
+}
+int execute(bool trace) {
+	SP = (1 << 23) -1;
+	int operand = 0, opcode = 0;
+	int instruction_num = 0;
+
+	while ((opcode = getMnemonicCode(memory[PC])) <= 18) {
+		
+		operand = getOperand(memory[PC]);
+
+		if (trace) printTrace(opcode, operand);
+		if (opcode == 18) break;
+		// Invalid Opcode
+		if (opcode > 18) return 1;
+
+		PC += 1;
+		executeInstruction(opcode, operand);
+		instruction_num += 1;
+	}
+	fprintf(stderr, "\n%d instructions executed.\n", instruction_num);
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    myfile.open(argv[2], ios::binary | ios::in);
     output.open("output.txt");
     read_file();
-    
+
+	
+
+	// If no arguments
+	if (argc < 3) {
+		printOptions();
+		return 1;
+	}
+
+	char* file_name = (char*) malloc(1024);
+	strcpy(file_name, argv[2]);
+	char* ptr = strchr(file_name, '.');
+	if (ptr != NULL) *ptr = '\0';
+
+	char* out_name = (char*) malloc(1024);
+	strcat(out_name, file_name);
+	strcat(out_name, "_memdump.txt");
+	
+
+	bool trace = (strcmp(argv[1], "-trace") == 0)? 1: 0;
+	bool before = (strcmp(argv[1], "-before") == 0)? 1: 0;
+	bool after = (strcmp(argv[1], "-after") == 0)? 1: 0;
+	bool ISA = (strcmp(argv[1], "-isa") == 0)? 1: 0;
+
+	// Invalid option
+	if (!(trace || before || after || ISA)) {
+		printOptions();
+		return 1;
+	}
+
+	int instructions_cnt = instructions.size();
+	// If -before then print
+	if (before) printMemory(instructions_cnt, 1);
+	if (ISA) isa();
+    int PC=0,t=0;
+    for(int u:instructions){
+        memory[t++]=u;
+    }
+	execute(trace);
+
+	if (after) printMemory(instructions_cnt, 0);
+	printMemoryToFile(instructions_cnt, output);
     myfile.close();
 }
